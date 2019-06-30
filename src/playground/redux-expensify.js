6eb2b1c71a16a472579797dbefc0c71fad413dbd
aaronshivers <aaron@moonmail.org>
2019-06-30 17:30:24 -0500

    setup sorting by date and amount

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -138,6 +138,12 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
     const textMatch = expense.description.toLowerCase().includes(text.toLowerCase())
 
     return startDateMatch && endDateMatch && textMatch
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1
+    }
   })
 }
 
@@ -163,10 +169,10 @@ const expense2 = store.dispatch(addExpense({ description: 'Coffee', amount: 300,
 
 // store.dispatch(editExpense(expense2.expense.id, { amount: 500 }))
 
-store.dispatch(setTextFilter('coffee'))
+// store.dispatch(setTextFilter('coffee'))
 // store.dispatch(setTextFilter())
 
-// store.dispatch(sortByAmount())
+store.dispatch(sortByAmount())
 // store.dispatch(sortByDate())
 
 // store.dispatch(setStartDate(0))

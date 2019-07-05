8341a8aa265a0e85b0ffbe2133e0e253972ac696
aaronshivers <aaron@moonmail.org>
2019-07-05 12:04:18 -0500

    changed port

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const publicPath = path.join(__dirname, '..', 'public')
+const { PORT } = process.env || 3000
 
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => res.sendFile(path.join(publicPath, 'index.html')))
 
-app.listen(3000, () => console.log(`Server Running.`))
+app.listen(PORT, () => console.log(`Server running on port ${ PORT }.`))

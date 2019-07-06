60458f956a89bdd81880879ec15348fb9ad9156f
aaronshivers <aaron@moonmail.org>
2019-07-06 08:29:53 -0500

    moved env variables to .env files

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,12 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHMbcTbDjzmVv0rj8pkSG8xKvuASwacFU",
-  authDomain: "expensify-146d1.firebaseapp.com",
-  databaseURL: "https://expensify-146d1.firebaseio.com",
-  projectId: "expensify-146d1",
-  storageBucket: "",
-  messagingSenderId: "131842579292",
-  appId: "1:131842579292:web:8169fb55d1e6258b"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 
 firebase.initializeApp(firebaseConfig)

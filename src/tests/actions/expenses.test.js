091bd755261f8c7880cf6253652b2dfdeeaba96e
aaron shivers <aaorn@moonmail.org>
2019-07-06 17:22:52 -0500

    setup delete from database

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -5,8 +5,10 @@ import {
   addExpense,
   editExpense,
   removeExpense,
+  startRemoveExpense,
   setExpenses,
-  startSetExpenses } from '../../actions/expenses'
+  startSetExpenses
+} from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
 
@@ -31,6 +33,25 @@ test('should setup remove expense action object', () => {
   expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
     id: '1234asdf'
+  })
+})
+
+test('should remove expense from firebase', done => {
+  const store = mockStore({})
+  const id = expenses[2].id
+
+  store.dispatch(startRemoveExpense({ id })).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'REMOVE_EXPENSE',
+      id
+    })
+    return database
+      .ref(`expenses/${ id }`)
+      .once('value')
+  }).then(snapshot => {
+    expect(snapshot.val()).toBeFalsy()
+    done()
   })
 })
 
